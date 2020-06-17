319f17e6cd097f2971b70a8ee9703c2f30f9ef44
yangming <472102644@qq.com>
2020-06-17 20:39:41 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import 'tcon/dist/reset.css'
 // 当前预览的卡片组件名
-const componentName = 'rivers-quality-28338153'
+const componentName = ''
 require(`./card-collection/${componentName}`)
 
 Vue.config.productionTip = false
